ef408e4adbbd2d0f7104c12db987da47502bdc07
Jeremy <jeremi.nunez@tecsup.edu.pe>
2019-06-12 12:50:40 -0500

    Initial commit

---
+++ app/src/main/java/com/bovisnunez/solicitudesapp/activities/MainActivity.java
@@ -0,0 +1,107 @@
+package com.bovisnunez.solicitudesapp.activities;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.bovisnunez.solicitudesapp.R;
+import com.bovisnunez.solicitudesapp.adapters.SolicitudAdapter;
+import com.bovisnunez.solicitudesapp.models.Solicitud;
+import com.bovisnunez.solicitudesapp.servicies.ApiService;
+import com.bovisnunez.solicitudesapp.servicies.ApiServiceGenerator;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private RecyclerView solicitudList;
+    private static final int REGISTER_FORM_REQUEST = 100;
+
+    public void showRegister(View view){
+        startActivityForResult(new Intent(this, RegisterActivity.class), REGISTER_FORM_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REGISTER_FORM_REQUEST) {
+            // refresh data
+            initialize();
+        }
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        solicitudList = findViewById(R.id.recyclerview);
+        solicitudList.setLayoutManager(new LinearLayoutManager(this));
+
+        solicitudList.setAdapter(new SolicitudAdapter());
+
+        initialize();
+
+
+
+    }
+
+    private void initialize() {
+
+        ApiService service = ApiServiceGenerator.createService(ApiService.class);
+
+        Call<List<Solicitud>> call = service.getProductos();
+
+        call.enqueue(new Callback<List<Solicitud>>() {
+            @Override
+            public void onResponse(Call<List<Solicitud>> call, Response<List<Solicitud>> response) {
+                try {
+
+                    int statusCode = response.code();
+                    Log.d(TAG, "HTTP status code: " + statusCode);
+
+                    if (response.isSuccessful()) {
+
+                        List<Solicitud> solicitudes = response.body();
+                        Log.d(TAG, "solicitudes: " + solicitudes);
+
+                        SolicitudAdapter adapter = (SolicitudAdapter) solicitudList.getAdapter();
+                        adapter.setSolicitudes(solicitudes);
+                        adapter.notifyDataSetChanged();
+
+                    } else {
+                        Log.e(TAG, "onError: " + response.errorBody().string());
+                        throw new Exception("Error en el servicio");
+                    }
+
+                } catch (Throwable t) {
+                    try {
+                        Log.e(TAG, "onThrowable: " + t.toString(), t);
+                        Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+                    }catch (Throwable x){}
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Solicitud>> call, Throwable t) {
+                Log.e(TAG, "onFailure: " + t.toString());
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+
+        });
+    }
+
+}
